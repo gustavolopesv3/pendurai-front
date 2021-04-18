1a052781a26d6209be3abe18b209321d06e05a32
Gustavo Lopes <38574162+gustavolopesv3@users.noreply.github.com>
2021-04-18 18:54:33 -0500

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { NavBar } from "./components/navbar";
+import { api } from "./services/api";
+
+interface ClientsData {
+  name: string;
+  cpf: string;
+}
 
 function App() {
+  const [clientes, setClientes] = useState<ClientsData[]>([]);
+
+  useEffect(() => {
+    getClients();
+  }, []);
+  const getClients = async () => {
+    const response = await api.get("/cliente");
+    setClientes(response.data);
+    console.log(response.data);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <NavBar />
+
+        <ul>
+          {clientes.map((cliente) => {
+            return (
+              <li key={cliente.cpf}>
+                {cliente.name} - {cliente.cpf}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,32 @@
+import style from "./navbar.module.scss";
+import {
+  RiGridFill,
+  RiStoreLine,
+  RiSearchLine,
+  RiArchiveLine,
+} from "react-icons/ri";
+export function NavBar() {
+  return (
+    <>
+      <nav className={style.navbar}>
+        <div className={style.logo}>
+          <h1>PendurAi !</h1>
+        </div>
+        <ul>
+          <li>
+            <RiGridFill /> <a href="">DashBoard</a>
+          </li>
+          <li>
+            <RiStoreLine /> <a href="">Venda</a>
+          </li>
+          <li>
+            <RiSearchLine /> <a href="">Clientes</a>
+          </li>
+          <li>
+            <RiArchiveLine /> <a href="">Produtos</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
